4051a1d2ab15d2cc599ee5f399dd24453fba9ff0
夏鹏程 <spectres@foxmail.com>
2021-11-02 18:51:25 +0800

    feat: 使用 import 导入图片资源

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,10 +5,10 @@
         <router-link :to="'/articles/' + item._id">
           <n-card :title="item.title" hoverable>
             <template #cover>
-              <img class="cover" src="src/assets/images/article-background.jpeg" draggable="false" />
+              <img class="cover" :src="articleBackgroundImageUrl" draggable="false" />
             </template>
             <section>
-              <n-avatar class="avatar" round size="large" src="src/assets/images/avatar.jpg" draggable="false" />
+              <n-avatar class="avatar" round size="large" :src="avatarImageUrl" draggable="false" />
             </section>
             {{ item.abstract }}
             <template #footer> footer</template>
@@ -22,6 +22,8 @@
 <script lang="ts" setup>
   // import { useMessage } from 'naive-ui';
   import { getArticlesApi } from '@/api/article/article';
+  import articleBackgroundImageUrl from '@/assets/images/article-background.jpeg';
+  import avatarImageUrl from '@/assets/images/avatar.jpg';
 
   // const message = useMessage();
   const { articles } = await getArticlesApi();
