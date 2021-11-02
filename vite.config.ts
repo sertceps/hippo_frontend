56807ede6a88485c3a714457692884577de5185f
夏鹏程 <spectres@foxmail.com>
2021-11-02 10:35:07 +0800

    codes style

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import path from 'path'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import path from 'path';
 
 const myResolve = (pathName: string) => {
-  return path.resolve(__dirname, pathName)
-}
+  return path.resolve(__dirname, pathName);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      { find: '@', replacement: myResolve('src') }
-    ]
-  }
-})
+    alias: [{ find: '@', replacement: myResolve('src') }],
+  },
+});
